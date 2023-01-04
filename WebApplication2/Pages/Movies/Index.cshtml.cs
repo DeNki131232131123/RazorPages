bc23278d792c9321a64f1d2a4a43ab55124ade98
Mateusz <mp110402@gmail.com>
2023-01-04 17:38:59 +0100

    Aktualizacja

---
+++ WebApplication2/Pages/Movies/Index.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using WebApplication2.Data;
+using WebApplication2.Models;
+
+namespace WebApplication2.Pages.Movies
+{
+    public class IndexModel : PageModel
+    {
+        private readonly WebApplication2.Data.WebApplication2Context _context;
+
+        public IndexModel(WebApplication2.Data.WebApplication2Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Movie> Movie { get;set; } = default!;
+        
+        [BindProperty(SupportsGet = true)]
+        public string? SearchString { get; set; }
+        public SelectList? genres { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string? MovieGenre { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            var movies = from m in _context.Movie
+                         select m;
+            if (!string.IsNullOrEmpty(SearchString))
+            {
+                movies = movies.Where(s => s.Title.Contains(SearchString));
+            }
+            
+                Movie = await _context.Movie.ToListAsync();
+        }
+    }
+}
